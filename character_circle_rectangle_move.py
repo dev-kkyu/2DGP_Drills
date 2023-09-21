9b3379b1dde9444df4bb8dad9d1a6c9171a579a5
devkkyu <dev.kkyu@gmail.com>
2023-09-21 10:13:38 +0900

    원 운동 완성

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -1,12 +1,26 @@
 from pico2d import *
+import math
 
 open_canvas()
 
 grass=load_image('grass.png')
-caracter = load_image('character.png')
+character = load_image('character.png')
 
 def run_circle():
 	print('CIRCLE')
+
+	# 일단 그림을 그리자
+
+	cx, cy, r = 400, 300, 200
+
+	for deg in range(0, 360, 1):
+		x = cx + r * math.cos(math.radians(deg))
+		y = cy + r * math.sin(math.radians(deg))
+		clear_canvas_now()
+		grass.draw_now(400, 30)
+		character.draw_now(x, y)
+		delay(0.01)
+
 	pass
 
 def run_rectangle():
@@ -16,6 +30,7 @@ def run_rectangle():
 while True:
 	run_circle()
 	run_rectangle()
+	break
 
 
 close_canvas()
