e65748fb08a390981b9542766d3cbdbedbe6985d
devkkyu <dev.kkyu@gmail.com>
2023-09-21 10:46:39 +0900

    원 속도조정

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -19,7 +19,7 @@ def run_circle():
 
 	cx, cy, r = 400, 300, 200
 
-	for deg in range(0, 360, 1):
+	for deg in range(0, 360, 3):
 		x = cx + r * math.cos(math.radians(deg))
 		y = cy + r * math.sin(math.radians(deg))
 		render_frame(x, y)
