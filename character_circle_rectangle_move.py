e98de0933d9bf86ab676b51d3043348a47106e27
devkkyu <dev.kkyu@gmail.com>
2023-09-21 10:35:19 +0900

    render_frame 분리

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -6,6 +6,12 @@ open_canvas()
 grass=load_image('grass.png')
 character = load_image('character.png')
 
+def render_frame(x, y):
+	clear_canvas_now()
+	grass.draw_now(400, 30)
+	character.draw_now(x, y)
+	delay(0.01)
+
 def run_circle():
 	print('CIRCLE')
 
@@ -16,27 +22,22 @@ def run_circle():
 	for deg in range(0, 360, 1):
 		x = cx + r * math.cos(math.radians(deg))
 		y = cy + r * math.sin(math.radians(deg))
-		clear_canvas_now()
-		grass.draw_now(400, 30)
-		character.draw_now(x, y)
-		delay(0.01)
-
-	pass
+		render_frame(x, y)
 
 def run_rectangle():
 	print('RECTANGLE')
 
 	for x in range(50, 750 + 1, 10):
-		clear_canvas_now()
-		grass.draw_now(400, 30)
-		character.draw_now(x, 90)
-		delay(0.01)
+		render_frame(x, 90)
+
+	# for x in range(750, 50, -10):
+	# 	render_frame(x, 90)
 
 	pass
 
 while True:
+	run_circle()
 	run_rectangle()
-	# run_circle()
 	break
 
 
