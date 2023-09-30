032f624b1e8eba78cc15e0e5507015100fd9a647
devkkyu <dev.kkyu@gmail.com>
2023-09-30 23:00:45 +0900

    왼쪽 오른쪽 방향구분, 프레임속도 관리

--- starter.py
+++ starter.py
@@ -33,10 +33,15 @@ def random_position():
 
 def go_hand():
 	global x, y, rx, ry
-	global is_arrived
+	global is_arrived, look
 
 	x1, y1 = x, y
 	x2, y2 = rx, ry
+
+	if x1 < x2:
+		look = 'right'
+	elif x1 > x2:
+		look = 'left'
 
 	t = 1 / 150
 	x = (1 - t) * x1 + t * x2
@@ -51,16 +56,21 @@ running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 rx, ry = x, y
 is_arrived = True
+look = 'right'
 frame = 0
+frame_time = 30
 # hide_cursor()
 
 while running:
 	clear_canvas()
 	TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-	hand_img.clip_composite_draw(0, 0, 50, 52, 0, 'h', rx, ry)
-	character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+	hand_img.clip_composite_draw(0, 0, 50, 52, 0, 'n', rx, ry)
+	if look == 'right':
+		character.clip_draw((frame // frame_time) * 100, 100 * 1, 100, 100, x, y)
+	elif look == 'left':
+		character.clip_draw((frame // frame_time) * 100, 100 * 0, 100, 100, x, y)
 	update_canvas()
-	frame = (frame + 1) % 8
+	frame = (frame + 1) % (8 * frame_time)
 	random_position()
 	go_hand()
 
