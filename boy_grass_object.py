e817839282a4b827347db548e16c7b5bb347aa84
devkkyu <dev.kkyu@gmail.com>
2023-10-05 10:29:59 +0900

    게임로직 구현

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,5 +1,6 @@
 from pico2d import *
 
+
 # Game object class here
 
 def handle_events():
@@ -11,11 +12,33 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+
+def reset_world():
+    global running
+
+    running = True
+
+
+def update_world():
+    pass
+
+
+def render_world():
+    clear_canvas()
+    update_canvas()
+
+
 open_canvas()
 
 # initialization code
+reset_world()
 
 # game main loop code
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
 
 # finalization code
 
